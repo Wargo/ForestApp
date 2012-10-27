e956cab5ee65b9040dedb0c9de98d5962dc53356
Guillermo <guillermo@artvisual.net>
2012-10-27 18:58:52 +0200

    mapa

--- Resources/ui/gps.js
+++ Resources/ui/gps.js
@@ -7,6 +7,32 @@ module.exports = function(home) {
 	var menu = MyMenu(win, home);
 	win.add(menu);
 	
+	var map = Ti.Map.createView({
+		top:45,
+		userLocation:false,
+		region:{latitude:39.5, longitude:-0.5, latitudeDelta:0.02, longitudeDelta:0.02}
+	});
+	
+	var annotation = Ti.Map.createAnnotation({
+		title:'hola',
+		subtitle:'adios',
+		pincolor:Ti.Map.ANNOTATION_PURPLE,
+		latitude:39.5,
+		longitude:-0.5,
+		animate:true,
+		leftButton:Ti.UI.iPhone.SystemButton.INFO_LIGHT
+	});
+	
+	map.addAnnotation(annotation);
+	
+	annotation.addEventListener('click', function(e) {
+		if (e.clicksource == 'leftButton') {
+		} else if (e.clicksource == 'pin') {
+		}
+	});
+	
+	win.add(map);
+	
 	return win;
 	
 }
